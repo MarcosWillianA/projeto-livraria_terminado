8c92b7ca58285a2895eaed77a952475174cfd05c
MarcosWillianlA <marcoswillianl1987@gmail.com>
2024-04-06 08:17:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projeto-livraria_terminado
+ Projeto de conclusão do curso de desenvolvimento web do IDEP
---
+++ script.js
@@ -0,0 +1,2 @@
+ü‘xt?ôñ66\ûõ'Û¶uïóA‹¶}–hØl{ÈFi'öt¨†}/¶ßGt¸¢Áöz®ß„cŒÕUúN.¼¥Š¨ïÝ{°3µ…fv2Ò(÷Üt“ÖG…ØÉO'©äÃóÛò§ý÷×6_íëóa8™±¹DbÝÕg]Ñ½m@¯DôIÇùëW„þù[²®SA¼ðè
+1š›	Óì»Ï&«lþuÄì¯˜<Çù¥é`ÌºNµÖŒyi†þ_«Q¾´_ÿò¦z:þã¿	˜ýCà‡„a:8¯oÒØ¯i4?6{hm¾Î$ëíd2xzíbS9¢kÏ˜
